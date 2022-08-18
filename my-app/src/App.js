60b2ea4d23fb35b096084d589bbd01afe1942ca2
Yama <goodyamaki.@mail.ru>
2022-08-18 14:24:23 +0300

    useMemo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useRef, useState} from "react";
-
+import {useEffect, useMemo, useRef, useState} from "react";
 
 
 function App() {
-// const [renderCount,setRenderCount]= useState(1)
-    const [value,setValue] = useState('initial')
-    const renderCount = useRef(1)
-    const inputRef = useRef(null)
-    const prevValue = useRef('')
+
+    const [number, setNumber] = useState(42)
+    const [colored, setColored] = useState(false)
 
 
 
-    useEffect(()=>{
-        renderCount.current ++
-        console.log(inputRef.current.value)
-    },)
+    const styles = useMemo(() =>({
+            color : colored ? ' darkred' : 'black'
 
-    useEffect(()=>{
-        prevValue.current = value
-    },[value])
+    }),[colored])
 
+    function complexCompute(num) {
+        let i = 0
+        while (i < 1000000000) i++
+        return num * 2
+    }
 
-    const focus = () => inputRef.current.focus()
+    const computed = useMemo(() => {
+        return complexCompute(number)
+    },[number])
 
-
+    useEffect(() =>{
+        console.log('Styles changed')
+    },[styles])
 
     return (
         <div>
-            <h1>Количество рендеров : {renderCount.current}</h1>
-            <h2>Прошлое состояние : {prevValue.current}</h2>
-            <input ref={inputRef} type='text' onChange={e=> setValue(e.target.value)} value={value}/>
-            <button onClick={focus}></button>
+            <h1 style={styles}>Svoistvo: {computed}</h1>
+            <button onClick={() => setNumber(prev => prev + 1)}>ADD</button>
+            <button onClick={() => setNumber(prev => prev - 1)}>REMOVE</button>
+            <button onClick={() => setColored(prev => !prev)}>Change</button>
         </div>
     );
 }
